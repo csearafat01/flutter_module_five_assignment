aceaf1f2ca3436f2babb47b2f9c4c5e2d1f376c3
csearafat01 <54732088+csearafat01@users.noreply.github.com>
2023-09-08 16:48:40 +0600

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
+<<<<<<< HEAD
   runApp(const MyApp());
 }
 
@@ -123,3 +124,90 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+=======
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(100),
+          child: AppBar(
+            backgroundColor: Colors.green,
+            elevation: 70,
+            leading: IconButton(
+              icon: Icon(Icons.add_business),
+              onPressed: () {
+                // Add your onPressed logic here
+              },
+            ),
+            actions: [
+              IconButton(
+                icon: Icon(Icons.search),
+                onPressed: () {
+                  // Add your onPressed logic here
+                },
+              ),
+            ],
+            title: Text(
+              'Home',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            centerTitle: true,
+          ),
+        ),
+        body: Center(
+            child:
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'This is mod 5 Assignment',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+                Text(
+                  '',
+                  style: TextStyle(
+                    fontSize: 10,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    RichText(
+                      text: TextSpan(
+                        style: TextStyle(fontSize: 20
+                        ),
+                        children: [
+                          TextSpan(
+                              text: 'My ', style: TextStyle(color: Colors.redAccent)
+                          ),
+                          TextSpan(text: 'phone ', style: TextStyle(fontSize: 14, color: Colors.blueAccent)
+                          ),
+                          TextSpan(text: 'name ', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.purpleAccent)
+                          ),
+                          TextSpan(text: 'Samsung Galaxy A30', style: TextStyle(color: Colors.amber)
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            )
+        ),
+      ),
+    );
+  }
+}
+>>>>>>> cb0445a (Initial commit)
